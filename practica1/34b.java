ed36ecdb7aedc59ea969a15b4532a83228d1017f
muzzidev <joaquin.hola.muzzi@gmail.com>
2025-03-19 12:15:18 -0300

    agrega otros ejercicio de meirda

--- practica1/34b.java
+++ practica1/34b.java
@@ -1,22 +1,12 @@
-// Un programa que tome dos argumentos enteros positivos e imprima “verdadero” si alguno de los argumentos
-// es divisible por el otro.
+// Un programa que imprima el valor de cos𝛼^2 + sen𝛼^2 para cualquier 𝛼 ingresado como argumento.
 
 package holamundo;
-
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
-		if(num1 % num2 == 0 || num2 % num1 == 0){
-			System.out.println("verdadero");
-		} else {
-			System.out.println("falso");
-		}
-		sc.close();
+		double num = 0.0000000069;
+		System.out.println(String.format("%.2f", num));
 	}
 
 }
